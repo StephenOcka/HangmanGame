c918ccb9939c3f05164284586a4e16cbcb170652
Esteban Padilla <98916874+StephenOcka@users.noreply.github.com>
2022-09-13 02:14:58 -0500

    Presentación tipo StarWars terminada.

---
+++ Resources/Js/Presentation.js
@@ -0,0 +1,15 @@
+/*----------------------Letter Effect-----------------------------*/
+let byline = document.getElementById("byLine");
+let bylineText = byline.innerHTML;
+let bylineArr = bylineText.split("");
+byline.innerHTML = "";
+for(i=0;i<bylineArr.length;i++){
+  let span = document.createElement("span");
+  let letter = document.createTextNode(bylineArr[i]);
+  if(bylineArr[i] == " ") {
+    byline.appendChild(letter);
+  } else {
+    span.appendChild(letter);
+    byline.appendChild(span);
+  }
+}
